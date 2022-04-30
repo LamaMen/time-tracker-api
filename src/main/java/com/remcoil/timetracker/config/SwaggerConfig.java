a1c80db524477739d6ece13fb8fbdb88415ced2c
Ilia Rodionov <ilia.r@nekidaem.ru>
2022-04-30 21:55:04 +0300

    [TIME-TRACKER-T-5] Add postgres docker and swagger configs

---
+++ src/main/java/com/remcoil/timetracker/config/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package com.remcoil.timetracker.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                version = "0.0.1",
+                title = "Time tracker",
+                description = "System for tracking the time of work on the project for RemCoil employees."
+        ),
+        servers = @Server(
+                url = "http://localhost:8080",
+                description = "Test server"
+        )
+)
+public class SwaggerConfig {
+}
