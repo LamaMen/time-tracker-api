3dee8fd2d93e6fb3301dac7a6179d80c36043f06
Ilia Rodionov <ilia.r@nekidaem.ru>
2022-06-10 14:50:00 +0300

    [TIME-TRACKER-T-5] Add test cors settings

--- src/main/java/com/remcoil/timetracker/TimeTrackerApplication.java
+++ src/main/java/com/remcoil/timetracker/TimeTrackerApplication.java
@@ -3,13 +3,16 @@ package com.remcoil.timetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class TimeTrackerApplication {
     @GetMapping("/")
+    @CrossOrigin(originPatterns = "*", methods = { RequestMethod.GET })
     @ResponseBody
     String home() {
         return "Hello World!";
