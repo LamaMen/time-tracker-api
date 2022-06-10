e496dab76e298e3e96434ef98a40281bc806cc5d
Ilia Rodionov <ilia.r@nekidaem.ru>
2022-06-10 14:11:40 +0300

    [TIME-TRACKER-T-5] Add readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+# TimeTracker Api
+
+Time Tracker is a service for tracking the time your team spent on different projects.
+
+TimeTracker supports the Swagger specification. It is located:
+```html 
+/swagger-ui/index.html
+```
--- src/main/java/com/remcoil/timetracker/TimeTrackerApplication.java
+++ src/main/java/com/remcoil/timetracker/TimeTrackerApplication.java
@@ -3,13 +3,13 @@ package com.remcoil.timetracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @SpringBootApplication
 public class TimeTrackerApplication {
-    @RequestMapping("/")
+    @GetMapping("/")
     @ResponseBody
     String home() {
         return "Hello World!";
