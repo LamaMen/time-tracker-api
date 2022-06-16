bd5040d539ef72b7dd01baa2ec47fd02b38da23f
Ilia Rodionov <ilia.rodionov.mail@gmail.com>
2022-06-16 15:14:26 +0300

    [TIME-TRACKER-T-1] Поправил миграцию, добавил изначального администратора

--- src/main/resources/db/migration/V0.0.1__create_users_table.sql
+++ src/main/resources/db/migration/V0.0.1__create_users_table.sql
@@ -1,10 +1,15 @@
-create schema time_tracker;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE TIME_TRACKER.USERS
+CREATE SCHEMA IF NOT EXISTS time_tracker;
+
+CREATE TABLE time_tracker.users
 (
-    id       uuid PRIMARY KEY,
+    id       uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     name     varchar(255) NOT NULL,
     surname  varchar(255) NOT NULL,
     password varchar(255) NOT NULL,
     role     varchar(60)  NOT NULL
 );
+
+INSERT INTO TIME_TRACKER.users (name, surname, password, role)
+VALUES ('Admin', 'Admin', 'protected_password', 'Admin');
